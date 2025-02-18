cb9172407846bab3d3490920e7a138b8e4c7c9d2
alucard2555 <168654870+alucard2555@users.noreply.github.com>
2025-02-18 07:47:25 -0300

    Add files via upload

---
+++ public class numerosgit{.java
@@ -0,0 +1,10 @@
+class inteiro {
+    public static void main(String[] args) {
+        int valor = 4;
+        int valor2 = 2;
+        if(valor > valor2)
+            System.out.println(valor );
+        else
+            System.out.println(valor2 );
+    }
+}
